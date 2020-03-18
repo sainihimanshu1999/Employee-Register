1a68e7ec37ced36a9e2a2dc2f7911d092ff43a52
Himanshu Saini <sainihimanshu.1999@gmail.com>
2020-03-18 18:48:28 +0530

    adding base, form, list pages

--- src/Employee/settings.py
+++ src/Employee/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'employee_register'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'Employee.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'EmployeeDB',
+        'USER' : 'postgres',
+        'PASSWORD' : 'Himanshu9716',
+        'HOST' : 'localhost'
+
     }
 }
 
--- src/employee_register/models.py
+++ src/employee_register/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+#Postion Model
+class Position(models.Model):
+    title = models.CharField(max_length = 50)
+
+#Employee Model
+class Employee(models.Model):
+    Fullname = models.CharField(max_length = 100 )
+    emp_code = models.CharField(max_length = 3 )
+    Mobile = models.CharField(max_length = 15 )
+    Position = models.ForeignKey(Position , on_delete = models.CASCADE)
